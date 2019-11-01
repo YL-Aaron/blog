e1b139d66d00e2479f5709a688bcee3256fc54ac
yilei <yilei@ruiec.cn>
2019-11-01 17:30:53 +0800

    增加特效

--- blog/src/main/java/com/yl/BlogApplication.java
+++ blog/src/main/java/com/yl/BlogApplication.java
@@ -3,8 +3,6 @@ package com.yl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author yi
--- blog/src/test/java/com/yl/controller/BlogApplicationTests.java
+++ blog/src/test/java/com/yl/controller/BlogApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.math.BigDecimal;
+
 public class BlogApplicationTests {
 
     @Test
     public void contextLoads() {
+       String title = "markdown\u7B80\u4ECB";
+        System.out.println(title);
     }
 
 }
